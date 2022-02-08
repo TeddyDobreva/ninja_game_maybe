0ebd4f4e2ef261f64ade69c5cfbbf24dc7d5380f
TeddyDobreva <95298135+TeddyDobreva@users.noreply.github.com>
2022-02-08 16:20:12 +0200

    Add files via upload

--- animations.js
+++ animations.js
@@ -1,81 +1,106 @@
-const Game = new Phaser.Game(1500, 700, Phaser.AUTO, 'GameCanvas', {preload,  create, update })
+const Game = new Phaser.Game(900, 700, Phaser.AUTO, 'GameCanvas', {preload,  create, update, test })
 
-ninjaSpeed=3
+let ninjaSpeed=3;
+let weaponSpeed=ninjaSpeed+2;
+let timerEvent;
+let br=0;
+max=4;
+let weapons_row1=[max];
+
 
 function preload(){
-    Game.load.spritesheet("ninja", "ninja_walk.png", 264/4,300/4)
-    Game.load.spritesheet("shuriken", "shuriken.png", 120/4, 25)
+    Game.load.spritesheet("ninja", "ninja_walk.png", 264/4,300/4);
+    Game.load.spritesheet("shuriken", "shuriken.png", 120/4, 25);
+    Game.load.image("weapon", "weapon.png");
 }
 
 function create(){
-    Game.stage.backgroundColor="00EEEE"
-    shuriken=Game.add.sprite(0, 0, "shuriken")
-    ninja=Game.add.sprite(Game.width/2, Game.height/2, "ninja")
-    ninja.frame=0
+    Game.stage.backgroundColor="00EEEE";
+    ninja=Game.add.sprite(Game.width/2, Game.height/2, "ninja");
+    ninja.frame=0;
 
-    ninja.animations.add("walk_forewords", [0,1,2,3], 8, true)
-    ninja.animations.add("walk_left", [4,5,6,7], 8, true)
-    ninja.animations.add("walk_right", [8,9,10,11], 8, true)
-    ninja.animations.add("walk_backwords", [13,14,15,12], 8, true)
-    shuriken.animations.add("rotate", [], 15, true).play()
+    ninja.animations.add("walk_forewords", [0,1,2,3], 8, true);
+    ninja.animations.add("walk_left", [4,5,6,7], 8, true);
+    ninja.animations.add("walk_right", [8,9,10,11], 8, true);
+    ninja.animations.add("walk_backwords", [13,14,15,12], 8, true);
 
-    keyUp = Game.input.keyboard.addKey(Phaser.Keyboard.UP)
-    keyDown = Game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
-    keyRight=Game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
-    keyLeft=Game.input.keyboard.addKey(Phaser.Keyboard.LEFT)
-    keyA = Game.input.keyboard.addKey(Phaser.Keyboard.A)
-    keyD = Game.input.keyboard.addKey(Phaser.Keyboard.D)
-    keyW = Game.input.keyboard.addKey(Phaser.Keyboard.W)
-    keyS = Game.input.keyboard.addKey(Phaser.Keyboard.S)
+    keyUp = Game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    keyDown = Game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    keyRight=Game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+    keyLeft=Game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    keyA = Game.input.keyboard.addKey(Phaser.Keyboard.A);
+    keyD = Game.input.keyboard.addKey(Phaser.Keyboard.D);
+    keyW = Game.input.keyboard.addKey(Phaser.Keyboard.W);
+    keyS = Game.input.keyboard.addKey(Phaser.Keyboard.S);
+
+    timerEvent=Game.time.events.add(Phaser.Timer.SECOND * 2, shoot, this);
+    timerEvent.loop=true;
 }
 
 function update(){
-shuriken.x+=5
-updatePosition()
+    if(br>0){ changeWeaponPosition();}
+updatePosition();
+}
+
+function changeWeaponPosition(){
+    for(let i=0; i<weapons_row1.length;i++){
+         weapons_row1[i].x+=3;
+    }
+   
+}
+
+function shoot(){
+    weapon=Game.add.sprite(-5557, 100, "weapon");
+    weapon.x=0-weapon.width
+    weapons_row1[br]=weapon;
+    br++;
 }
 
 function updatePosition(){
     if(keyUp.isDown||keyW.isDown){
 
-        if(ninja.y<=0){ninja.y=0}
+        if(ninja.y<=0){ninja.y=0;}
         
         else{
-        ninja.y -= ninjaSpeed
-        ninja.animations.play("walk_backwords")
+        ninja.y -= ninjaSpeed;
+        ninja.animations.play("walk_backwords");
     }
     }
     
     if(keyDown.isDown||keyS.isDown){
-        if(ninja.y>=Game.height-ninja.height){ninja.y=Game.height-ninja.height
+        if(ninja.y>=Game.height-ninja.height){ninja.y=Game.height-ninja.height;
         }
         else{
-       ninja.y +=ninjaSpeed
-       ninja.animations.play("walk_forewords")
+       ninja.y +=ninjaSpeed;
+       ninja.animations.play("walk_forewords");
     }
     }
 
     if(keyLeft.isDown||keyA.isDown){
 
-        if(ninja.x<=0){ninja.x=0}
+        if(ninja.x<=0){ninja.x=0;}
         
         else 
         {
-            ninja.animations.play("walk_left")
-        ninja.x -= ninjaSpeed
+            ninja.animations.play("walk_left");
+        ninja.x -= ninjaSpeed;
     }
     }
     
     if(keyRight.isDown||keyD.isDown){
-        if(ninja.x>=Game.width-ninja.width){ninja.x=Game.width-ninja.width
+        if(ninja.x>=Game.width-ninja.width){ninja.x=Game.width-ninja.width;
         }
         else{
-       ninja.x +=ninjaSpeed
-       ninja.animations.play("walk_right")
+       ninja.x +=ninjaSpeed;
+       ninja.animations.play("walk_right");
     }
     }
 
     if((!((keyD.isDown||keyRight.isDown)||(keyUp.isDown||keyW.isDown)||(keyLeft.isDown||keyA.isDown)||(keyDown.isDown||keyS.isDown)))||((keyDown.isDown||keyS.isDown)&&(keyUp.isDown||keyW.isDown))||((keyLeft.isDown||keyA.isDown)&&(keyRight.isDown||keyD.isDown))){
-        ninja.frame=12
+        ninja.frame=12;
     }
 
+}
+function test(){
+console.log("walk_left");
 }
