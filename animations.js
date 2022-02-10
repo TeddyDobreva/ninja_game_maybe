19bdd260be075622390fd849c6ea42cb72892dd0
TeddyDobreva <95298135+TeddyDobreva@users.noreply.github.com>
2022-02-10 15:45:52 +0200

    Add files via upload

--- animations.js
+++ animations.js
@@ -1,12 +1,13 @@
 const Game = new Phaser.Game(1600, 1000, Phaser.AUTO, 'GameCanvas', {preload,  create, update })
 
 let ninjaSpeed=3;
-let weaponSpeed=ninjaSpeed+0.5;
+let weaponSpeed=ninjaSpeed;
 let timerEvent;
 let br=0;
 let weapons_per_row=[];
 
 function preload(){
+    Game.load.spritesheet("game_over", "game_over.png", 1110/3,300);
     Game.load.spritesheet("ninja", "ninja_walk.png", 264/4,300/4);
     Game.load.spritesheet("shuriken", "shuriken.png", 120/4, 25);
     Game.load.image("weapon", "weapon.png");
@@ -65,7 +66,14 @@ updatePosition();
 function checkCol(){
     for(i=0; i<weapons_per_row.length;i++){
     if(Game.physics.arcade.collide(ninja ,weapons_per_row[i])==true){
-        console.log("game over")
+    ninja.destroy()
+
+    timerEvent.loop=false;  
+    game_over=Game.add.sprite(Game.width/2,Game.height/2, "game_over");
+    game_over.x=(Game.width-game_over.width)/2;
+    game_over.y=(Game.height-game_over.height)/2;
+    game_over.animations.add("push", [0,1,2,1,0], 10, false);
+
     }
 }}
 
@@ -86,7 +94,7 @@ function changeWeaponPosition(){
 function shoot(){
     for( i=1; i<num_of_rows-1; i++ ){
     weapon=Game.add.sprite(-5557, 50, "weapon");
-    weapon.x=0-weapon.width-Math.floor(Math.random() * 6)*weapon.width
+    weapon.x=0-weapon.width-Math.floor(Math.random() * 10)*weapon.width
     weapon.y=i*(ninja.height+weapon.height)+ninja.height/2
     Game.physics.enable(weapon, Phaser.Physics.ARCADE);
     weapons_per_row[br]=weapon;
